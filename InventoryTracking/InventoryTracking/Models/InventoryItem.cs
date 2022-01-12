d6b4d5028dfd7b257716a920d5d713f032aea157
akorcsak <62719168+akorcsak@users.noreply.github.com>
2022-01-11 19:54:25 -0500

    Created Inventory Model

---
+++ InventoryTracking/InventoryTracking/Models/InventoryItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InventoryTracking.Models
+{
+    public class InventoryItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Item_name { get; set; }
+
+        public string Stock { get; set; }
+
+        public string Price { get; set; }
+    }
+}
